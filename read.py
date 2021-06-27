c92883e4a2ef44a3dcc7879c32f5684b3b64ec4f
Stanley <stanley0823.lee@gmail.com>
2021-06-27 16:16:50 +0800

    calcualte

--- read.py
+++ read.py
@@ -7,3 +7,14 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print(len(data))
+
+sum_len = 0
+for li in data:
+	sum_len += len(li)
+
+print('留言平均長度為',sum_len/len(data))
+
+	
+
+
+	
